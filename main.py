66d583b2ee631bbcf2a89cab8c805ad59f160c07
Maia <106710526+cpMaia@users.noreply.github.com>
2022-06-22 18:20:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+nome=input("Qual seu nome?")
+print("olá,",nome)
+print("olá,mundo")
+
+idade= input("Qual a sua idade?")
+
+if int (idade)> 18:
+  print("Você é um adulto")
+  print("Muito obrigado")
+else:
+  print("você é uma criança")
+  print("Tudo bem?")
+  
